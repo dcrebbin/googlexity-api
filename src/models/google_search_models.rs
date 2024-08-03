70330b2964a5eb3c7455ed7e428d796c1769b2b0
Devon Crebbin <devon@artvuu.group>
2024-08-04 03:50:41 +1000

    fixed search response parsing

--- src/models/google_search_models.rs
+++ src/models/google_search_models.rs
@@ -137,6 +137,10 @@ pub struct CseImage {
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Person {
-    pub name: String,
-    pub url: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub name: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub url: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub org: Option<String>,
 }
